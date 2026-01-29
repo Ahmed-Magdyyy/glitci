1d33269d9bf14fb6c4bcdfdcddf73a43ca6686cd
Ahmed Magdy <76597137+Ahmed-Magdyyy@users.noreply.github.com>
2026-01-29 07:08:22 +0200

    cors update

--- src/modules/auth/auth.controller.js
+++ src/modules/auth/auth.controller.js
@@ -16,7 +16,9 @@ import {
 const refreshTokenCookieOptions = {
   httpOnly: true,
   secure: process.env.NODE_ENV === "production",
-  sameSite: "strict",
+  // sameSite: "strict",
+  sameSite: "none",
+  secure: true,
   maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
 };
 
